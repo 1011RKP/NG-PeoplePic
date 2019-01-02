5547037fdd07f8fff4c08d48d07585b874d5a59d
Ratna Paleru (non-Incyte) <RPaleru@incyte.com>
2019-01-02 13:11:24 -0500

    Changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PeoplePickerComponent } from './people-picker/people-picker.component';
+import { SPService } from './services/sp.service';
+
+@NgModule({
+  declarations: [AppComponent, PeoplePickerComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AutoCompleteModule,
+    HttpClientModule
+  ],
+  providers: [SPService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+// ng build --deploy-url https://incyte.sharepoint.com/sites/ratnadev/SiteAssets/PP/ --base-href https://incyte.sharepoint.com/sites/ratnadev/Pages/People-Picker.aspx
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  web: 'https://incyte.sharepoint.com/sites/ratnadev/'
+};
